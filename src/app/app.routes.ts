2177cb323da791b2bf589642c836d5480c3e5109
Ashton Chingovo <ashton.chingovo@entelect.co.za>
2025-09-10 23:01:04 +0200

    Add modifications to sign-in ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PastMeetupsComponent } from './pages/past-meetups/past-meetups.component';
-import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { AuthComponent } from './pages/auth/auth.component';
 
 export const routes: Routes = [
